ac4bf53a968745bf31f90b7f53a1894fe1531991
josh coffman <josh@computeristsolutions.com>
2013-11-22 22:20:10 -0700

    added old routes for old links

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,8 @@ Csinc::Application.routes.draw do
   # match 'blog/categories/:name' => 'categories#show', :as => :full_category
   get '/sitemap.xml' => 'sitemap#index'
   
-  # match 'blog/syndication.axd' => 'feed#rss', :as => :rss_posts
-  # match 'blog/author/*other' => 'home#about'
-  # match 'blog/posts/:year/:month/:day/:slug' => 'posts#show'
+  get 'blog/syndication.axd' => 'feed#rss', :as => :rss_posts
+  get 'blog/author/*other' => 'home#about'
+  get 'blog/posts/:year/:month/:day/:slug' => 'posts#show'
   get 'blog/posts/:year/:month/:slug' => 'posts#show', :as => :full_post
 end
