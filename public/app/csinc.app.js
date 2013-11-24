5b16c93a78799a298b91603b3d69d0f558ed6ecd
josh coffman <josh@computeristsolutions.com>
2013-11-23 23:54:37 -0700

    added angular routes and views

--- public/app/csinc.app.js
+++ public/app/csinc.app.js
@@ -1,14 +1,26 @@
-var csinc = angular.module('csinc', ['csinc.resources.posts']);
+var csinc = angular.module('csinc', ['ngRoute', 'csinc.resources.posts']);
 
-// csinc.config(['$routeProvider', function ($routeProvider) {
-//    $routeProvider.when('/home', { templateUrl: '/app/home.html' })
-//    $routeProvider.when('/about', { templateUrl: '/app/about.html' })
-//    $routeProvider.when('/contact', { templateUrl: '/app/contact.html' })
-//    $routeProvider.when('/blog', { templateUrl: '/app/blog.html' })
-//    .otherwise({ redirectTo: '/home' });
-// }]);
+csinc.config(['$routeProvider', function ($routeProvider) {
+   $routeProvider.when('/home', { templateUrl: '/app/views/home.html' })
+   $routeProvider.when('/about', { templateUrl: '/app/views/about.html' })
+   $routeProvider.when('/contact', { templateUrl: '/app/views/contact.html' })
+   $routeProvider.when('/blog', { templateUrl: '/app/views/blog.html', controller: 'BlogCtrl' })
+   .otherwise({ redirectTo: '/home' });
+}]);
 
-csinc.controller('AppCtrl', ['$scope', '$rootScope', '$location', 'Posts', function ($scope, $rootScope, $location, Posts) { 
+csinc.controller('AppCtrl', ['$scope', 'Posts', function ($scope, Posts) { 
+        (function init() { 
+            console.log('init');
+            // Posts.list(1, function(data) {
+            //     console.log(JSON.stringify(data));
+            // });
+            // Posts.get('mongoid-and-csinc', function(data) {
+            //     console.log(JSON.stringify(data));
+            // });
+        })(); 
+    }]);
+
+csinc.controller('BlogCtrl', ['$scope', 'Posts', function ($scope, Posts) { 
         (function init() { 
             console.log('init');
             // Posts.list(1, function(data) {
