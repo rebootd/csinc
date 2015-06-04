6e1d4f1913884deda7f25595b68c65d7f60584c6
josh coffman <rebootd@computeristsolutions.com>
2015-06-04 11:07:40 -0700

    tweaking content

--- public/app/csinc.app.js
+++ public/app/csinc.app.js
@@ -18,7 +18,7 @@ csinc.run(['$location', '$rootScope', function($location, $rootScope) {
 csinc.factory('DateService', ['$http', function ($http) {
 
         var DateService = {};
-        DateService.monthNames = [ "January", "February", "March", "April", "May", "June", 
+        DateService.monthNames = [ "January", "February", "March", "April", "May", "June",
             "July", "August", "September", "October", "November", "December" ];
         DateService.formatDate = function(dt) {
             if(!dt) {return '';}
@@ -34,50 +34,43 @@ csinc.factory('DateService', ['$http', function ($http) {
 
     }]);
 
-csinc.controller('AppCtrl', ['$scope', '$location', 'Posts', 'DateService', function ($scope, $location, Posts, DateService) { 
+csinc.controller('AppCtrl', ['$scope', '$location', 'Posts', 'DateService', function ($scope, $location, Posts, DateService) {
         $scope.page = 'home';
-
-        $scope.$on('$viewContentLoaded', function(event) {
-            setTimeout(function() {
-                //console.log('#'+$location.$$path);
-                clicky.log('#'+$location.$$path);
-            }, 100);
-        });
 
         $scope.setPage = function (page) {$scope.page = page;};
         $scope.getDateArray = function(postDate) {
             return DateService.getDateArray(postDate);
         };
         $scope.getLink = function (post) {
-            var ar = $scope.getDateArray(post.published_on);            
+            var ar = $scope.getDateArray(post.published_on);
             return '/#/blog/posts/' + ar[0] + '/' + ar[1] + '/' + post.slug;
         };
         $scope.isActive = function(path) {
             return $location.$$path.substring(0,path.length) == path;
         };
 
-        (function init() { 
+        (function init() {
             // console.log('init AppCtrl');
-        })(); 
+        })();
     }]);
 
-csinc.controller('BlogCtrl', ['$scope', 'Posts', 'DateService', function ($scope, Posts, DateService) { 
+csinc.controller('BlogCtrl', ['$scope', 'Posts', 'DateService', function ($scope, Posts, DateService) {
         $scope.list = [];
         $scope.currentPage = 1;
         $scope.hasMore = false;
-        $scope.monthNames = [ "January", "February", "March", "April", "May", "June", 
+        $scope.monthNames = [ "January", "February", "March", "April", "May", "June",
             "July", "August", "September", "October", "November", "December" ];
 
         $scope.trimmedBody = function(body) {
             if(!body || body.length == 0) {return '{empty}';}
             var str = body.replace(/(<[^>]*>)|\n|\t/g,'');
-            
+
             if(str.length < 200) {return str;}
             return str.substring(0,198) + '..';
         };
 
-        $scope.prevPage = function() { 
-            if($scope.currentPage==1) {return;} 
+        $scope.prevPage = function() {
+            if($scope.currentPage==1) {return;}
             $scope.currentPage--;
             Posts.list($scope.currentPage, function(data) {
                 $scope.list = data;
@@ -85,7 +78,7 @@ csinc.controller('BlogCtrl', ['$scope', 'Posts', 'DateService', function ($scope
             });
         };
         $scope.nextPage = function() {
-            if(!$scope.hasMore) {return;} 
+            if(!$scope.hasMore) {return;}
             $scope.currentPage++;
             Posts.list($scope.currentPage, function(data) {
                 $scope.list = data;
@@ -99,31 +92,31 @@ csinc.controller('BlogCtrl', ['$scope', 'Posts', 'DateService', function ($scope
             return DateService.formatDate(dt);
         };
 
-        (function init() { 
+        (function init() {
             // console.log('init BlogCtrl');
             Posts.list($scope.currentPage, function(data) {
                 $scope.list = data;
                 $scope.hasMore = $scope.list.length>0;
             });
-        })(); 
+        })();
     }]);
 
-csinc.controller('PostCtrl', ['$rootScope', '$scope', '$routeParams', 'Posts', 'DateService', function ($rootScope, $scope, $routeParams, Posts, DateService) { 
+csinc.controller('PostCtrl', ['$rootScope', '$scope', '$routeParams', 'Posts', 'DateService', function ($rootScope, $scope, $routeParams, Posts, DateService) {
         $scope.post = {};
         $scope.slug = '';
-        $scope.monthNames = [ "January", "February", "March", "April", "May", "June", 
+        $scope.monthNames = [ "January", "February", "March", "April", "May", "June",
             "July", "August", "September", "October", "November", "December" ];
 
         $scope.formatDate = function(dt) {
             return DateService.formatDate(dt);
         };
 
-        (function init() { 
+        (function init() {
             // console.log('init PostCtrl');
             $scope.slug = typeof($routeParams.slug) == "undefined" ? 'slug' : $routeParams.slug;
             Posts.get($scope.slug, function(data) {
                 $scope.post = data;
                 $rootScope.title = $scope.post.title;
             });
-        })(); 
+        })();
     }]);
