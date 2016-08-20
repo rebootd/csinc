e27a926221605d14563d51b12064803592fcc674
rebootd <rebootd@computeristsolutions.com>
2016-08-19 21:15:30 -0700

    remove blog routes and redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,27 @@
 Csinc::Application.routes.draw do
-  get "posts/index"
-  get "posts/show"
-  get "posts/get"
+  # get "posts/index"
+  # get "posts/show"
+  # get "posts/get"
   root 'home#index'
 
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
   get 'about' => 'home#index', as: :about
   get 'contact' => 'home#index', as: :contact
 
-  get "feed/rss" => 'feed#rss'
-  get "feed/json" => 'feed#json'
-  get "feed/:slug" => 'feed#show'
-  get "feed/all" => 'feed#all'
-  scope "/blog" do
-    resources :posts
-  end
-  get 'blog' => 'posts#index'
-  # match 'blog/categories/:name' => 'categories#show', :as => :full_category
+  # get "feed/rss" => 'feed#rss'
+  # get "feed/json" => 'feed#json'
+  # get "feed/:slug" => 'feed#show'
+  # get "feed/all" => 'feed#all'
+  # scope "/blog" do
+    # resources :posts
+  # end
+  get 'blog', to: redirect('/')
+  get 'posts', to: redirect('/')
   get '/sitemap.xml' => 'sitemap#index'
 
-  get 'blog/syndication.axd' => 'feed#rss', :as => :rss_posts
-  get 'blog/author/*other' => 'home#about'
-  get 'blog/posts/:year/:month/:day/:slug' => 'posts#show'
-  get 'blog/post/:slug' => 'posts#show'
-  get 'blog/posts/:year/:month/:slug' => 'posts#show', :as => :full_post
+  # get 'blog/syndication.axd' => 'feed#rss', :as => :rss_posts
+  # get 'blog/author/*other' => 'home#about'
+  # get 'blog/posts/:year/:month/:day/:slug' => 'posts#show'
+  # get 'blog/post/:slug' => 'posts#show'
+  # get 'blog/posts/:year/:month/:slug' => 'posts#show', :as => :full_post
 end
