d03467f08b21e1bed4f3cf80b529aa2469f6afa9
rebootd <rebootd@computerist.co>
2019-05-18 19:51:44 -0700

    improved template support

--- filegen.rb
+++ filegen.rb
@@ -9,28 +9,38 @@ require 'json'
 #
 # 
 # 
-
+	
 sourcepath = '../../../Dropbox/notes/sitecontent'
 outpath = './'
 mapfile = "#{sourcepath}/contentmap.json"
 mdfiles = Dir["#{sourcepath}/*.md"]
-default_template = "#{sourcepath}/default_template.html"
 markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
 
 map_content = File.read(mapfile)
 contentmap = JSON.parse(map_content)
 
+default_template = "#{sourcepath}/#{contentmap["template.file"]}"
+pages = contentmap["pages"]
+
 puts 'starting..'
 robots = "User-agent: * \nDisallow: /cgi-bin/"
 
-template_source = File.read(default_template)
+site_template_source = File.read(default_template)
 
-contentmap.each do | entry |
+puts contentmap["site.title"]
+puts contentmap["site.description"]
+
+pages.each do | entry |
   in_filename = "#{sourcepath}/#{entry["file"]}"
   puts "processing: #{in_filename}"
   md_source = File.read(in_filename)
   html_output = Markdown.new(md_source).to_html
-  page_output = template_source.gsub("{{ page.body }}", html_output).gsub("{{ page.title }}", entry["title"]).gsub("{{ page.keywords }}", entry["keywords"])
+  page_template_source = entry["template.file"] ? File.read("#{sourcepath}/#{entry["template.file"]}") : site_template_source
+  page_output = page_template_source.gsub("{{page.body}}", html_output).gsub("{{page.title}}", entry["page.title"])
+  	.gsub("{{page.keywords}}", entry["page.keywords"])
+  	.gsub("{{page.description}}", entry["page.description"])
+  	.gsub("{{site.title}}", contentmap["site.title"])
+  	.gsub("{{site.description}}", contentmap["site.description"])
   output_name = File.basename(in_filename, ".*") + ".html"
   puts "...writing: #{output_name}"
   File.write("#{output_name}", page_output)
@@ -42,6 +52,6 @@ File.write("robots.txt", robots + "\n")
 # publish
 system 'git add *.html'
 system 'git commit -m "updating site content"'
-# system 'git push'
+system 'git push'
 
 puts 'done!'
